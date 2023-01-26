fd84ec6c077fadab3203a332e7541c9ef044bd57
Juanma <x232187@gruposantander.es>
2023-01-26 17:37:19 +0100

    wip class

---
+++ 3-javascript/08-class/app.js
@@ -0,0 +1,12 @@
+// Clases en JS
+class PersonClass {
+    constructor( name, surname ) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
+let personClass1 = new PersonClass( 'Juanma', 'Molins' );
+console.log( personClass1 );
+let personClass2 = new PersonClass( 'Carolina', 'Perez' );
+console.log( personClass2 );
+// Metodo get y set
