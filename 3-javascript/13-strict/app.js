9272ec2a8277e61d0a55656ae07537fadc8f7dd5
Juanma <x232187@gruposantander.es>
2023-02-02 14:03:51 +0100

    fixed strict file

--- 3-javascript/13-strict/app.js
+++ 3-javascript/13-strict/app.js
@@ -8,7 +8,11 @@ console.log( x );
 // myFunction() {}
 function myFunction() {
     "use strict";
-    x = 10;
+    // La variable x la tenemos declarda fuera de la función por eso no nos tira ningún error y nos devuelve su valor
+    x = 25;
     console.log( x );
+    // y = 10;
+    let y = 5;
+    console.log( y );
 }
 myFunction();
