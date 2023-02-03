47a3469648872ee4a35fba65aca006cddd8d74bd
Juanma <x232187@gruposantander.es>
2023-02-03 12:53:38 +0100

    wip arrow functions

---
+++ 3-javascript/16-arrow-function/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    console.log( `Saludos desde mi función` );
+}
+myFunction();
+let myFunctionArrow = () => {
+    console.log( `Saludos desde mi función de flecha` );
+}
+myFunctionArrow();
